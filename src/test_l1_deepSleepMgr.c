b57a09108aa0c92cb7f102fe276349f314985fcc
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2023-10-09 11:30:03 -0400

    Update gTestID for initial test

--- src/test_l1_deepSleepMgr.c
+++ src/test_l1_deepSleepMgr.c
@@ -1,23 +1,4 @@
 /**
-*  If not stated otherwise in this file or this component's Licenses.txt
-*  file the following copyright and licenses apply:
-*
-*  Copyright 2022 RDK Management
-*
-*  Licensed under the Apache License, Version 2.0 (the License);
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an AS IS BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
-
-/**
  * @addtogroup HPK HPK
  * @{
  * @addtogroup DEEPSLEEPMGR DEEPSLEEPMGR
@@ -79,6 +60,7 @@ static int gTestID = 1;
  */
 void test_l1_deepSleepMgr_positive_PLAT_DS_INIT (void)
 {
+    gTestID = 1;
     UT_LOG("\n In %s [%02d%03d]\n", __FUNCTION__, gTestGroup, gTestID);
 	// Define expected results
     const DeepSleep_Return_Status_t expectedInit = DEEPSLEEPMGR_SUCCESS;
